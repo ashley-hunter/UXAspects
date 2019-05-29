5a47b614f40928390efda876a4bb0eede34eadc7
Ashley Hunter <ashley-glenn.hunter@microfocus.com>
2019-05-29 16:21:23 +0100

    Temporarily removing sankey chart

--- docs/app/pages/charts/charts-sections/sankey-chart/sankey-charts.module.ts
+++ docs/app/pages/charts/charts-sections/sankey-chart/sankey-charts.module.ts
@@ -1,12 +1,11 @@
 import { ComponentFactoryResolver, NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { FileSizePipeModule, HybridModule, SankeyChartModule, TabsetModule } from '@ux-aspects/ux-aspects';
+import { FileSizePipeModule, HybridModule, TabsetModule } from '@ux-aspects/ux-aspects';
 import { DocumentationComponentsModule } from '../../../../components/components.module';
 import { DocumentationCategoryComponent } from '../../../../components/documentation-category/documentation-category.component';
 import { DocumentationPage, ResolverService } from '../../../../services/resolver/resolver.service';
 import { WrappersModule } from '../../../../wrappers/wrappers.module';
 import { ChartsSankeyChartNg1Component } from './sankey-chart-ng1/sankey-chart-ng1.component';
-import { ChartsSankeyChartComponent } from './sankey-chart/sankey-chart.component';
 
 
 const SECTIONS = [
@@ -31,7 +30,6 @@ const ROUTES = [
         HybridModule,
         FileSizePipeModule,
         DocumentationComponentsModule,
-        SankeyChartModule,
         RouterModule.forChild(ROUTES)
     ],
     exports: SECTIONS,
